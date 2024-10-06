9be3f6df810dba865fccf93aeff4ba8e585db19f
MANU0519 <anishkushwaga1116@gmail.com>
2024-10-06 09:38:21 +0545

    push

--- frontend/src/component/Login.jsx
+++ frontend/src/component/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
         <div className="mx-auto max-w-lg">
           <h1 className="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">Get started today</h1>
 
-          <p className="mx-auto mt-4 max-w-md text-center text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati sunt dolores deleniti inventore quaerat mollitia?</p>
+          <p className="mx-auto mt-4 max-w-md text-center text-gray-500">Login & yeild</p>
 
           <form onSubmit={handleSubmit(onSubmit)} className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8">
             <p className="text-center text-lg font-medium">Sign in to your account</p>
